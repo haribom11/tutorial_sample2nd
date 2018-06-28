79daae5866c0c341333686bcb7daafefe6d20d8e
noma <harinezumilove1106@gmail.com>
2018-06-28 19:57:18 +0900

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -8,7 +8,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   test "should get home" do
     get static_pages_home_url
-    assert_response :success
+    assert_response :successgit 
     assert_select "title","Home | #{@base_title}"
   end
 
