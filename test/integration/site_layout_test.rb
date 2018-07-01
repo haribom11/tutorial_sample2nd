567e61e130584cd8c9fb310d246061c8926b2b80
noma <harinezumilove1106@gmail.com>
2018-07-01 21:36:02 +0900

    -7.24

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -13,5 +13,5 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "title", full_title("Contact")
     get signup_path 
     assert_select "title", full_title("Sign up")
-  end
+    end
 end
