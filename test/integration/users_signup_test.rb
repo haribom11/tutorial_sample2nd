1092cbe47b31e990052ddab348582cd4c6b83dd4
noma <harinezumilove1106@gmail.com>
2018-07-02 12:00:24 +0900

    -7.27

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,5 +11,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert'
+    assert_select 'div.alert-danger'
+    assert_select 'form[action="/signup"]'
   end
 end
