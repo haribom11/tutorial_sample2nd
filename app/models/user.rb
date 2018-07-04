93aa5b620526342d8a6845cd89ca1d8f448c2657
noma <harinezumilove1106@gmail.com>
2018-07-04 09:42:20 +0900

    -9.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    attr_accessor :remember_token
     before_save { email.downcase! }
     validates :name,  presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -8,9 +9,21 @@ class User < ApplicationRecord
     has_secure_password
     validates :password,presence: true, length: { minimum: 6}
 
+    #渡された文字列のハッシュ値を返す
     def User.digest(string)
      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST:
      BCrypt::Engine.cost
      BCrypt::Password.create(string,cosrt: cost)
+    end
+
+    #ランダムなトークンを返す
+    def User.new_token
+     SecureRandom.urlsafe_base64
+    end
+
+    #永続セッションのためにユーザーをDBに記憶する
+    def remember
+     self.remember_token = User.new_token
+     update_attribute(:remember_digest,User.digest(remember_token))
     end
 end
