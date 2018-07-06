a8269b5a2adad73f76332f4d6bc96ca03df76023
noma <harinezumilove1106@gmail.com>
2018-07-06 14:06:55 +0900

    -9.23

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  
   def new
   end
 
@@ -6,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      remember user
+      params[:session][:remember_me] == '1'? remember(user) :forget(user)
       redirect_to user
      else
       flash.now[:danger] = 'Invalid email/password combination'
