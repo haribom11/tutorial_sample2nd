eba9b88307709418e97dd4c192a4020c6a9f56a8
noma <harinezumilove1106@gmail.com>
2018-07-06 13:29:39 +0900

    -9.20

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,7 +29,8 @@ class User < ApplicationRecord
 
     #渡されたトークンがダイジェストと一致したらtrueを返す
     def authenticated?(remember_token)
-     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+        return false if remember_digest.nil?
+        BCrypt::Password.new(remember_digest).is_password?(remember_token)
     end
 
     #ユーザーのログイン情報を破棄する
