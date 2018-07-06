658aaf188727f34b0ce45466e8c5de132ca167ba
noma <harinezumilove1106@gmail.com>
2018-07-06 16:31:13 +0900

    -9.24

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,7 +10,23 @@ class ActiveSupport::TestCase
   fixtures :all
   include ApplicationHelper
   
+  #テストユーザーがログイン中の場合にtrueを返す
   def is_logged_in?
     !session[:user_id].nil?
   end
+
+  #テストユーザーとしてログインする
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+end
+
+class ActionDispatch::IntegrationTest
+
+  #テストユーザーとしてログインする
+  def log_in_as(user,password: 'password',remember_me:'1')
+    post login_path,params:{session: { email: user.email, 
+      password: password, remember_me: remember_me }}
+  end
+  
 end
