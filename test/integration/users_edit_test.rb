a30b7b46c0b0b8e47b37a0188c33204f28d159f7
noma <harinezumilove1106@gmail.com>
2018-07-13 00:32:25 +0900

    10.22

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -32,4 +32,17 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_equal name,  @user.name
     assert_equal email, @user.email
   end
+
+  test "should redirect edit when not logged in" do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update when not logged in" do
+    patch user_path(@user),params:{user:{name:@user.name,
+                          email: @user.email}}
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
 end
