0fcbc254a03471c223d03537767532103d86126e
noma <harinezumilove1106@gmail.com>
2018-07-13 08:18:35 +0900

    10.33

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,7 +10,12 @@ module SessionsHelper
      user.remember
      cookies.permanent.signed[:user_id] = user.id
      cookies.permanent[:remember_token] = user.remember_token
-    end 
+    end
+
+    #渡されたユーザーがログイン済みユーザーであればユーザーを記憶する
+    def current_user?(user)
+        user == current_user
+    end
     
     #記憶トークンcookieに対応するユーザーを返す
     def current_user
@@ -25,6 +30,7 @@ module SessionsHelper
         end
       end
     end
+
     #ユーザーがログインしていればtrue、その他ならfalseを返す
     def logged_in?
         !current_user.nil?
@@ -41,5 +47,16 @@ module SessionsHelper
         forget(current_user)
         session.delete(:user_id)
         @current_user = nil
+    end
+
+    #記憶したURL（もしくはデフォルト値）にリダイレクト
+    def redirect_back_or(default)
+        redirect_to(session[:forwarding_url] || default)
+        session.delete(:forwarding_url)
+    end
+
+    #アクセスしようとしたURLを覚えておく
+    def store_location
+        session[:forwarding_url] = request.original_url if request.get?
     end
 end
